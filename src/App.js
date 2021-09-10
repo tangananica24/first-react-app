b46b71a77cfc2d76cd7ceb74dc497d3fdf0e47bd
andrea_daza <tangananica24@gmail.com>
2021-09-09 20:37:24 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = {
+    first: "Andrea",
+    last: "Daza"
+  }
+  const inputPlaceholder = "Enter details";
+  const age = "23";
+  const job = "engineer";
+
+  const getFullName = (firstName, lastName) => `${firstName} ${lastName}`
+
+  const getInput = () => <input placeholder={inputPlaceholder}/>
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Full Name: {getFullName(name.first, name.last)}</h3>
+      <p>Age: {age}</p>
+      <p>Job: {job}</p>
+
+    {getInput()}
+    <br/>
+    {
+      name.first === "Andrea" ? "true" : "false"
+    }
     </div>
   );
 }
